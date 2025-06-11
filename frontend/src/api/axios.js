72697a76a528b334484a67832144e8746082b028
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-06-11 11:07:04 +0530

    changed the deployed backend BaseURL in the frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://mern-agent-management-3.onrender.com' });
+const API = axios.create({ baseURL: 'https://mern-agent-management-3.onrender.com/api' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
